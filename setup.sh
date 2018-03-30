4d9e201e300b46beb40b9ea1686799f9eff5b3dd
Remington Campbell <code@remington.io>
2018-03-29 20:48:28 -0700

    Add Docker installation check to setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+which docker &> /dev/null
+if [ $? -eq 0 ]; then
+    echo "Docker is installed!"
+else
+    echo "Please install Docker before running!"
+    exit 1
+fi
 docker swarm init
